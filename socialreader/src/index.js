22522836b30330ef81ae59bfcd7e02bcb065408c
Angel <amartinezo04@educarex.es>
2025-03-24 22:27:21 +0100

    Inicio de Sesion

--- socialreader/src/index.js
+++ socialreader/src/index.js
@@ -1,6 +1,4 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 //Configuracion de la web app de Firebase
 const firebaseConfig = {
@@ -9,10 +7,9 @@ const firebaseConfig = {
   projectId: "social-reader-9b7a2",
   storageBucket: "social-reader-9b7a2.firebasestorage.app",
   messagingSenderId: "288679986999",
-  appId: "1:288679986999:web:53634e20c2ca04e4a5fcb2",
-  measurementId: "G-N1QZN2GFN3"
+  appId: "1:288679986999:web:507818b8bb451a85a5fcb2",
+  measurementId: "G-QEHQRTVDQ0"
 };
 
 // Inicioalizo AppFirebase y otros modulos
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
